9348f72bbebb01dd1d20fcdc2037c631cbb0f937
Ted Moke <theodore.moke@gmail.com>
2016-09-19 12:41:31 -0400

    Added custom input functionality

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,13 @@
-var time, current, hours, minutes, seconds;
+var time, current, hours, minutes, seconds, customHr, customMin, customSec;
 
 function addZero(n) {
   return (n < 10 ? "0" + n : n);
 }
 
 function timerStart() {
+  document.querySelector("#customHr").value = "";
+  document.querySelector("#customMin").value = "";   
+  document.querySelector("#customSec").value = "";   
   current = new Date();
   hours = addZero(current.getHours());
   minutes = addZero(current.getMinutes());
@@ -22,6 +25,17 @@ function timerStart() {
 }
 
 function setEnd() {
+  customHr = (document.querySelector('#customHr').value).replace(/\s/g, '');
+  customMin = (document.querySelector('#customMin').value).replace(/\s/g, '');
+  customSec = (document.querySelector('#customSec').value).replace(/\s/g, '');
+
+  if (customHr != "" && !isNaN(customHr) && customHr < 24)
+  {hours = customHr;}
+  if (customMin != "" && !isNaN(customMin) && customMin < 60)
+  {minutes = customMin;}
+  if (customSec != "" && !isNaN(customSec) && customSec < 60)
+  {seconds = customSec;}
+
   if (hours >= 23 && minutes >= 42) {
     document.getElementById("intermissionEnd").innerHTML = "00" + ":" + addZero((parseInt(minutes) + 18) - 60) + ":" + seconds;
   } else if (hours < 23 && minutes >= 42) {
@@ -33,6 +47,13 @@ function setEnd() {
 
 function timerReset() {
   document.getElementById("intermissionEnd").innerHTML = "-";
+  clearInput();          
+}
+
+function clearInput() {
+  document.querySelector("#customHr").value = "";
+  document.querySelector("#customMin").value = "";   
+  document.querySelector("#customSec").value = "";   
 }
 
 
