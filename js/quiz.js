28bef7808b11455486b78019fdd68740e231b031
theodore.moke <theodore.moke@gmail.com>
2016-12-13 14:52:52 -0500

    Finished core quiz function, complete cycle

--- js/quiz.js
+++ js/quiz.js
@@ -1,52 +1,100 @@
 "use strict";
+
 function globalFunction() {
   var order = [];
+  var submit = document.getElementById('btnSubmit');
+  var start = document.getElementById('btnStart');
+  var activeQuiz = document.getElementById('activeQuiz');
+  var question = document.getElementById('question');
+  var answer = document.getElementById('answer');
+  var scoreCount = document.getElementById('score');
+  var final = document.getElementById('final');
+  var score = 0, asked = 0;
   const questions = {
     0: {
-      stadium: 'Prudential Center',
-      city: 'Newark',
+      stadium: 'prudential center',
+      city: 'newark',
       team: 'Devils'
     },
     1: {
-      stadium: 'TD Garden',
-      city: 'Boston',
+      stadium: 'td garden',
+      city: 'boston',
       team: 'Bruins'
     },
     2: {
-      stadium: 'Madison Square Garden',
-      city: 'New York City',
+      stadium: 'madison square garden',
+      city: 'new york city',
       team: 'Rangers'
     },
     3: {
-      stadium: 'Barclays Center',
-      city: 'Brooklyn',
+      stadium: 'barclays center',
+      city: 'brooklyn',
       team: 'Islanders'
     },
     4: {
-      stadium: 'Bell Centre',
-      city: 'Montreal',
+      stadium: 'bell centre',
+      city: 'montreal',
       team: 'Canadiens'
     }
   }
-  
+
   function startQuiz(questions) {
+    score = 0;
+    asked = 0;
     let quizLength = Object.keys(questions).length;
-    while (order.length < quizLength)
-      {
-        let x = Math.floor(Math.random() * (quizLength));
-        if (order.indexOf(x) === -1) { order.push(x);}
+    while (order.length < quizLength) {
+      let x = Math.floor(Math.random() * (quizLength));
+      if (order.indexOf(x) === -1) {
+        order.push(x);
       }
+    }
+    showElt(activeQuiz);
+    hideElt(start);
     displayNextQuestion(questions, order);
   }
-  
+
   function displayNextQuestion(questionSet, order) {
     //console.log(questionSet.msg.stadium)
-    console.log(questionSet[order[0]].team);
-    order.shift();
-    if (order.length > 0) {displayNextQuestion(questions,order);}
+    question.innerHTML = "Where do the " + questionSet[order[0]].team + " play?";
   }
 
-  startQuiz(questions);
+  function scoreQuestion(order) {
+    asked++;
+    if (answer.value.toLowerCase() === questions[order[0]].stadium) {
+      score++;
+    }
+    scoreCount.innerHTML = "Score: " + score + " correct out of " + asked;
+    answer.value = '';
+    order.shift();
+    if (order.length > 0) {
+      displayNextQuestion(questions, order);
+    } else {
+      endQuiz();
+    }
+  }
+
+  function endQuiz() {
+    hideElt(activeQuiz);
+    showElt(start);
+    showElt(final);
+    final.innerHTML = "Your answered " + score + " out of " + asked + " questions correctly (%" + (score/asked*100) + ")!";
+    start.innerHTML = "Play again";
+  }
+
+  function showElt(elt) {
+    elt.className = elt.className.replace(/(?:^|\s)hidden(?!\S)/g, '');
+  }
+
+  function hideElt(elt) {
+    elt.className += "hidden";
+  }
+
+  start.addEventListener('click', function() {
+    startQuiz(questions)
+  });
+  submit.addEventListener('click', function() {
+    scoreQuestion(order)
+  });
 }
 
 globalFunction();
