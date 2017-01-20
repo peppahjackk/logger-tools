fccad8badba6be7c2371bd0e98a27621fb87d897
theodore.moke <theodore.moke@gmail.com>
2017-01-20 11:25:18 -0500

    Added css sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   return gulp.src('css/**/*.css')
+    .pipe(sourcemaps.init())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
   }))
+    .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('dist'))
 });
